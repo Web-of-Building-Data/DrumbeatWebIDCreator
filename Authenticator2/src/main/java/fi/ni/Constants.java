070009ce063cb662d7991dc779e66b3dd6b8decc
jyrkio <Jyrki.Oraskari@aalto.fi>
2017-02-16 13:11:11 +0200

    New project for the security

--- Authenticator2/src/main/java/fi/ni/Constants.java
+++
@@ -1,6 +0,0 @@
-package fi.ni;
-
-public class Constants {
-    public static final String RDF_filePath="C:/var/drumbeat_admin/";
-    public static final String property_base="https://drumbeat.cs.hut.fi/";
-}
